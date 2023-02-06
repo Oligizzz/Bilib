5abb5ec20166cf5dba890d55e8fbb4bbca973556
宫水-米子哈 <face_kobe59@163.com>
2023-02-07 00:33:34 +0800

    Zuul

---
+++ Zuul/src/main/java/com/mailisu/zuulserver/filtes/BaseFilter.java
@@ -0,0 +1,38 @@
+package com.mailisu.zuulserver.filtes;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author KongXiao
+ * @date 2023-02-05 16:26
+ */
+@Component
+public class BaseFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        System.out.println(request);
+        return null;
+    }
+}
